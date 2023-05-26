d201cdeab038cda6239be9b44d9fee9d324e0443
chang10th <chang10thomas@gmail.com>
2023-05-26 17:30:15 +0200

    Final push

--- PROGRAMME/webpack test/src/main.js
+++ PROGRAMME/webpack test/src/main.js
@@ -1,9 +1,37 @@
 import "./main.scss"
 
-function test(param1){
-    console.log(param1);
+const teacherApiKey = "b37c6858cb58515d7a40e37c3b01bbd9";
+const myApiKey = "d117952ed469d4ca1e5b049ba79878c4";
+
+/**
+fetch("test.txt")
+.then(function(response){
+    return response.text()
+})
+.then(function(data){
+    console.log(data)
+});
+
+
+async function getWeatherInfo(city){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${teacherApiKey}&mode=html`);
+    const data = await response.text();
+    return data;
+}
+*/
+
+function display(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${teacherApiKey}&mode=html`)
+    .then(function(response){
+        return response.text()
+    })
+    .then(function(data) {
+        console.log(data);
+        document.getElementById('weatherInfo').innerHTML = data;
+    })
 }
 
-test("Fromage");
-test("Camembert");
-test("Chaussée aux moines");
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+
+btn.addEventListener("click", display);
