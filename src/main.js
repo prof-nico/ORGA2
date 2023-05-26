8889b89ab4bd3900ba830d71cc940c6489d2ed29
Guillaume Delépine <guillaume.delepine74@orange.fr>
2023-05-26 12:39:25 +0200

    ok

---
+++ src/main.js
@@ -0,0 +1,8 @@
+let nom = 'delepine';
+let prenom = 'guillaume'
+console.log(prenom);
+let hello = function (){
+    let presentation = `Je m'appelle ${prenom} ${nom} et j'ai 25ans.`;
+    return presentation;
+}
+alert(hello())
